c1cd675cb737001182023c5e3b85f3fdf83af466
jesuschua <jesuschuajr@gmail.com>
2018-08-14 14:57:16 +0800

    Halfway there
    Signed-off-by: jesuschua <jesuschuajr@gmail.com>

--- app/src/main/java/com/example/jesuschuajr/taplock/MainActivity.java
+++ app/src/main/java/com/example/jesuschuajr/taplock/MainActivity.java
@@ -14,6 +14,9 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -33,41 +36,63 @@ public class MainActivity extends AppCompatActivity {
         });
         Button tapbutton = findViewById(R.id.button);
         tapbutton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-
     }
 
     int tapcount = 0;
     long startTime, difference, nextTime;
+    ArrayList<Long> tapArr = new ArrayList<Long>();
 
     public void buttonOnClick(View v){
+
         TextView textView = findViewById(R.id.textView);
-        if (tapcount == 0){
-            long startTime = System.nanoTime();
+        Switch switch1 = findViewById(R.id.switch1);
+        if (switch1.isChecked()){
+            if (tapcount == 0){
+                startTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime());
+                //textView.setText("Start");
+            }
+            else{
+                nextTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime());
+                difference = nextTime - startTime;
+                textView.setText(String.valueOf(difference));
+                tapArr.add(difference);
+                startTime = nextTime;
+            }
+            tapcount++;
         }
         else{
-            long nextTime = System.nanoTime();
-            long difference = nextTime - startTime;
-            textView.setText(String.valueOf(difference));
-            startTime = nextTime;
+            textView.setText("Locked");
         }
-        tapcount++;
 
     }
 
     public void buttonOnClick2(View v){
-        TextView textView = findViewById(R.id.textView);
-        tapcount = 0;
-        textView.setText("Locked");
+        Switch switch1 = findViewById(R.id.switch1);
+        Button button = findViewById(R.id.button);
+
+        if (switch1.isChecked()){
+            TextView textView = findViewById(R.id.textView);
+            tapcount = 0;
+            textView.setText(String.valueOf(tapArr));
+            button.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            switch1.setChecked(false);
+
+        }
+        else{
+            tapcount = 0;
+        }
+        tapArr = new ArrayList<Long>();
+
     }
 
     public void buttonOnClickToggle(View v){
         Button button = findViewById(R.id.button);
         Switch switch1 = findViewById(R.id.switch1);
+        TextView textView = findViewById(R.id.textView);
         if (switch1.isChecked()){
             button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
         } else button.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-
-
+        textView.setText("Locked");
     }
 
     @Override
@@ -83,12 +108,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
